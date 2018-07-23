1dc76a1208852990a3b60f5aad90d91a1eb9e1fe
Mashuk-EIS <mushfiqurrahman.eis@gmail.com>
2018-07-23 11:47:01 +0600

    Migrations ride_bookings,ride_requests,ride_offers table

---
+++ database/migrations/2018_07_23_054013_change_ride_offers_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeRideOffersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('ride_offers', function (Blueprint $table) {
+            $table->integer('request_id')->after('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
