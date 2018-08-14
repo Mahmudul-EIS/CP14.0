8632575761940279161d1c9218349cd28691f5ed
Mahmudul-EIS <35486228+Mahmudul-EIS@users.noreply.github.com>
2018-08-14 15:37:22 +0600

    Add Jquery ui css file

--- resources/views/frontend/includes/head.blade.php
+++ resources/views/frontend/includes/head.blade.php
@@ -15,6 +15,8 @@
     <link rel="stylesheet" href="{{ asset('public/assets/frontend/css/bootstrap-select.css') }}">
     <!-- datepicker css -->
     <link rel="stylesheet" href="{{ asset('public/assets/frontend/css/bootstrap-datetimepicker.css') }}">
+     <!-- datepicker css -->
+    <link rel="stylesheet" href="{{ asset('public/assets/frontend/css/jquery-ui-1.10.4.smoothness.css') }}">
     <!-- style css -->
     <link rel="stylesheet" href="{{ asset('public/assets/frontend/css/style.css') }}">
     <!-- mordernizr css -->
